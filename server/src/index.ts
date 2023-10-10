6b171fe39118c0cc8c096f6b67f44bada305a3b0
njayman <najishm828282@gmail.com>
2023-10-10 14:47:17 +0600

    added bun, server and client

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia();
+
+app.get("/", () => "Hello Elysia");
+app.listen(3000);
+
+console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
